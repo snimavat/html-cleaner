88c20ebe48a9d936babb2216649792ae1be90aa1
sudhir nimavat <sudhir@nimavat.me>
2016-10-13 00:05:21 +0530

    - upgrade plugin to grails 255 - Upgrade to soup 1.9.2 - add travis integration

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,10 @@
-grails.project.work.dir = 'target'
+if (System.getenv('TRAVIS')) {
+	grails.project.repos.grailsCentral.username = System.getenv('GRAILS_CENTRAL_USERNAME')
+	grails.project.repos.grailsCentral.password = System.getenv('GRAILS_CENTRAL_PASSWORD')
+}
 
+grails.project.work.dir = '.grails'
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
 
 	inherits 'global'
@@ -12,12 +17,10 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		compile 'org.jsoup:jsoup:1.8.1'
+		compile 'org.jsoup:jsoup:1.9.2'
 	}
 
 	plugins {
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
-			export = false
-		}
+		build(":release:3.1.2", ":rest-client-builder:2.1.1") { export = false } 
 	}
 }
---
+++ travis-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf *.zip
+
+./grailsw refresh-dependencies --non-interactive
+./grailsw test-app --non-interactive
+./grailsw package-plugin --non-interactive
+./grailsw maven-install --non-interactive
+
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "snimavat/html-cleaner" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+	./grailsw publish-plugin --allow-overwrite --non-interactive
+
+else
+  echo "Not on master branch, so not publishing"
+  echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+fi	
+
+
